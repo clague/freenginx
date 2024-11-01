b6a0b70deead174783cd9e254c2065952eb73b65
root <root@nice-flags-1.localdomain>
2024-10-31 20:10:38 -0700

    update

---
+++ objs/ngx_http_auth_pam_module_modules.c
@@ -0,0 +1,22 @@
+
+#include <ngx_config.h>
+#include <ngx_core.h>
+
+extern ngx_module_t  ngx_http_auth_pam_module;
+
+ngx_module_t *ngx_modules[] = {
+    &ngx_http_auth_pam_module,
+    NULL
+};
+
+char *ngx_module_names[] = {
+    "ngx_http_auth_pam_module",
+    NULL
+};
+
+char *ngx_module_order[] = {
+    "ngx_http_auth_pam_module",
+    "ngx_http_access_module",
+    NULL
+};
+
